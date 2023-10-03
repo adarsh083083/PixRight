1c658475e33281234150ddeb0db5087d91a67fcf
adarsh <adarsh083083@gmail.com>
2023-10-04 02:14:17 +0530

    worked on payment ui and dashboard ui

--- src/screens/PaymentScreen/PaymentScreen.jsx
+++ src/screens/PaymentScreen/PaymentScreen.jsx
@@ -1,11 +1,41 @@
-import { View, Text } from "react-native";
+import { View, Text, StatusBar, StyleSheet } from "react-native";
 import React from "react";
+import BackgroundImage from "../../components/BackgroundImage/ScreenBackground";
+import { IMAGE } from "../../constants/Images";
+import CustomHeader from "../../components/HeaderAtom/HeaderAtom";
+import AppButton from "../../components/ButtonAtom/ButtonAtom";
+import styles from "./PaymentScreenStyle";
 
 const PaymentScreen = () => {
   return (
-    <View>
-      <Text>PaymentScreen</Text>
-    </View>
+    <BackgroundImage source={IMAGE.BLACK_IMAGE}>
+      <StatusBar
+        animated={false}
+        backgroundColor="transparent"
+        barStyle="light-content"
+        translucent={true}
+        backgroundImage={IMAGE.STATUS_BAR_BACKGROUND}
+      />
+      <View style={styles.headerStyle}>
+        <CustomHeader backImage={IMAGE.BACK_ARROW} title={"PIXY RIGHT"} />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.titleStyle}>Free Member</Text>
+        <Text numberOfLines={3} style={styles.textStyle2}>
+          You are a free member.we are offering you a trial version of this
+          app.your trial will expire withen 3 days
+        </Text>
+      </View>
+      <View style={styles.btnStyle}>
+        <AppButton btnText={"UNLIMITED WATERMARK FOR 40.99/MONTH"} />
+      </View>
+      <View style={styles.btnStyle2}>
+        <AppButton btnText={"UNLIMITED LOGOS FOR 40.99/MONTH"} />
+      </View>
+      <View style={styles.btnStyle2}>
+        <AppButton btnText={"RESTORE YOUR SUBSCRIPTION 40.99/MONTH"} />
+      </View>
+    </BackgroundImage>
   );
 };
 
--- src/screens/SetUpScreen/SetUpScreen.jsx
+++ src/screens/SetUpScreen/SetUpScreen.jsx
@@ -1,11 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, StatusBar } from "react-native";
 import React from "react";
+import BackgroundImage from "../../components/BackgroundImage/ScreenBackground";
+import { IMAGE } from "../../constants/Images";
+import CustomHeader from "../../components/HeaderAtom/HeaderAtom";
 
 const SetUpScreen = () => {
   return (
-    <View>
-      <Text>SetUpScreen</Text>
-    </View>
+    <BackgroundImage source={IMAGE.BLACK_IMAGE}>
+      <StatusBar
+        animated={false}
+        backgroundColor="transparent"
+        barStyle="light-content"
+        translucent={true}
+        backgroundImage={IMAGE.STATUS_BAR_BACKGROUND}
+      />
+      <View style={{ marginTop: 30 }}>
+        <CustomHeader backImage={IMAGE.BACK_ARROW} title={"PIXY RIGHT"} />
+      </View>
+    </BackgroundImage>
   );
 };
 
